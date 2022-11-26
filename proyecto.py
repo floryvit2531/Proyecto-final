b623f8f99664931c5ba522ffcb70abb55294ce28
Franco Miguez <franco.miguez98@gmail.com>
2022-11-26 13:53:06 -0300

    devolución

--- proyecto.py
+++ proyecto.py
@@ -4,13 +4,14 @@ from PIL import Image, ImageTk
 
 
 def abrir_imagen():
-    global imagen
+    global imagen, img
     archivo= filedialog.askopenfilename(filetypes=[("Image", ("*.jpg"))])
-    img=Image.open("C:\\Users\\marif\\Downloads\\flores-bonitas.jpg")
-    img.thumbnail((300,300))
-    img= ImageTk.PhotoImage(img)
-    imagen=tk.Label(ventana,image=img)
-    imagen.image=img
+    img=Image.open(archivo)
+    img_tk = img
+    img_tk.thumbnail((300,300))
+    img_tk= ImageTk.PhotoImage(img_tk)
+    imagen=tk.Label(ventana,image=img_tk)
+    imagen.image=img_tk
     imagen.pack(pady=30)
     
     
@@ -24,11 +25,11 @@ frame_azul = tk.Frame (ventana, width=100, height=200)
 frame_azul.configure(background= "blue")
 frame_azul.pack()
 
-img2=Image.open("C:\\Users\\marif\\Downloads\\abrir_img.jpg")
+img2=Image.open("abrir_img.png")
 img2.thumbnail((50,50))
 img2= ImageTk.PhotoImage(img2)
 
-img3=Image.open("C:\\Users\\marif\\Downloads\\rotar_imagen.jpg")
+img3=Image.open("rotar_imagen.png")
 img3.thumbnail((50,50))
 img3= ImageTk.PhotoImage(img3)
 
@@ -37,8 +38,9 @@ texto=tk.Label(master=frame_azul,text="Mi Proyecto")
 texto.pack()
 
 def girar_imagen():
-    img4=Image.open("C:\\Users\\marif\\Downloads\\flores-bonitas.jpg")
-    img4=img4.rotate(45)
+    global img
+    img = img.rotate(45)
+    img4 = img
     imgtk=ImageTk.PhotoImage(img4)
     imagen.image=imgtk
     imagen.configure(image=imgtk)
